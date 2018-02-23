888e66df2a17902e333bf8bddabdbb6645368e51
Solution Science <marshnjagi@yahoo.com>
2018-02-23 15:14:09 +0300

    changed to proc2

--- src/server/app.js
+++ src/server/app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.get('*', function(req, res){ return res.sendFile(path.join(__dirname, 'public/index.html'))})
+// app.get('*', function(req, res){ return res.sendFile(path.join(__dirname, 'public/index.html'))})
 
 
 
